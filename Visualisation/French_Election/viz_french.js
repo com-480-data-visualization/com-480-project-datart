a907fe479c8aa51bcba737771f261861ed2439f6
JeremyBensoussan <38009920+JeremyBensoussan@users.noreply.github.com>
2020-04-09 17:00:26 +0100

    Update French
    Shadowinig - tooltip update - bug fix on reigo

--- Visualisation/French_Election/viz_french.js
+++ Visualisation/French_Election/viz_french.js
@@ -34,8 +34,10 @@ const projectionMayotte = d3.geoEquirectangular()
     .translate([40, 470]);
 
 var matching = {FN : "#273568", LR : "#0058A2", UMP : "#0058A2", LREM :"#FCB731", Modem : "#FCB731", PS : "#FF8080", LFI: "#CA472B"};
+var matchingDark = {FN : "#131a33", LR : "#002b50", UMP : "#002b50", LREM :"#936202", Modem : "#936202", PS : "#be0000", LFI: "#642315"};
 var legendText = ["Extreme Right", "Right" , "Center",  "Left", "Extreme Left"];
 var legendColor = ["#273568 ", "#0058A2" , "#FCB731",  "#FF8080", "#CA472B"];
+var matchingCandidates = {FI: "LR", MÉ : "LFI", LE:"FN", CH: "UMP", JO :"PS", RO: "PS", HO : "PS", SA : "LR", MA : "LREM"}
 
 var div = d3.select("body").append("div")   
     .attr("class", "tooltip")               
@@ -59,7 +61,7 @@ function draw(){
             .data(geojson.features)
             .enter()    
             .append("path")
-            .attr('class', "regions")
+            .attr('class', function(d){return d.properties.nom[0] + d.properties.nom[1]  + d.properties.nom[2];})
             .style("fill", function(d){
                 switch(sliderStep.value()) {    
                 case 2017:
@@ -113,7 +115,33 @@ function draw(){
             })
            
 
-            .on("mouseover", function(d) {  
+            .on("mouseover", function(d) {
+                st = "." + d.properties.nom[0] + d.properties.nom[1]  + d.properties.nom[2]
+               
+                deps.select(st).style("fill", function(d){
+                switch(sliderStep.value()) {    
+                case 2017:
+                  return matchingDark[d.properties["Parti"]]; 
+                  break;
+                  
+                case 2012:
+                  return matchingDark[d.properties["Parti0"]]; 
+                  break;
+                  
+                case 2007:
+                   return matchingDark[d.properties["Parti1"]]; 
+                  break;
+                  
+                case 2002:
+                  return matchingDark[d.properties["Parti2"]]; 
+                  break;
+                  
+                default:
+                  return matchingDark[d.properties["Parti"]];
+                  break;
+                }
+            })
+
                 div.transition()        
                     .duration(200)
                     .style("opacity", 1);
@@ -161,17 +189,55 @@ function draw(){
                 var format = d3.format(".2f")
 
               
-               div.html(d.properties.nom +"<br/>"   
-                + "1st : " + d.properties[surnameFirst] + " : " + format(d.properties[firstScore])+"% <br/>"
-                + "2nd : " + d.properties[surnameSecond] + " : " + format(d.properties[secondScore])+"% <br/>")  
+               div.html( '<h3> '+ d.properties.nom+ "</h3>"+ 
+                "<ul><li id = 'firstCandidate'>"+  d.properties[surnameFirst] + " : " + format(d.properties[firstScore])+"% </li>"
+                + "<li id = 'secondCandidate'> "+ d.properties[surnameSecond] + " : " + format(d.properties[secondScore])+"% </li></ul>")  
                     .style("left", (d3.event.pageX + 30) + "px")     
                     .style("top", (d3.event.pageY - 30) + "px")
-      
+
+                div.select("#firstCandidate").style("border-left",function(){
+                    temp =d.properties[surnameFirst][0] + d.properties[surnameFirst][1]
+                    color = matching[matchingCandidates[temp]]
+                    return "solid " + color;
+                })
+
+                 div.select("#secondCandidate").style("border-left",function(){
+                    temp =d.properties[surnameSecond][0] + d.properties[surnameSecond][1]
+                    color = matching[matchingCandidates[temp]]
+                    return "solid " + color;
+                })
 
             }) 
             .on("mouseout", function(d) {
-                div.style("opacity", 0);
-                div.html("")
+                st = "." + d.properties.nom[0] + d.properties.nom[1]  + d.properties.nom[2]
+                deps.select(st)    
+                .style("fill", function(d){
+                switch(sliderStep.value()) {    
+                case 2017:
+                  return matching[d.properties["Parti"]]; 
+                  break;
+                  
+                case 2012:
+                  return matching[d.properties["Parti0"]]; 
+                  break;
+                  
+                case 2007:
+                   return matching[d.properties["Parti1"]]; 
+                  break;
+                  
+                case 2002:
+                  return matching[d.properties["Parti2"]]; 
+                  break;
+                  
+                default:
+                  return matching[d.properties["Parti"]];
+                  break;
+                }
+            })
+               
+
+                div.style("opacity", 0)
+             
                 .style("left", "-500px")
                 .style("top", "-500px");
             });
@@ -212,12 +278,10 @@ data = [2002,2007,2012,2017]
 
 function appendTitle(){
     deps.append("text")
-        .attr("x", (width / 2))
+        .attr("x", 20)
         .attr("y", 25)
-        .attr("text-anchor", "middle")
+        .attr("text-anchor", "left")
         .style("fill", "dark")
-        .style("font-weight", "300")
-        .style("font-size", "19px")
         .text("Results 1st round of French election per region 2002 - 2017");
 
     deps.append("text")
